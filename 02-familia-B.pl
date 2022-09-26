5621bb873a7e610d7446455dba32eb1d15b088a0
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-25 22:06:54 -0300

    update

--- 02-familia-B.pl
+++ 02-familia-B.pl
@@ -7,38 +7,46 @@
 % Aluno: Rômulo Souza Fernandes
 %-------------------------------------------------
  % FATOS
-
-    mulher(luisa).  % significa luisa é uma mulher
+ 
+    mulher(luisa).
     mulher(eva).
     mulher(martha).
+    mulher(maria).
+    mulher(suzana).
     homem(adam).
     homem(pedro).
     homem(paulo).
-    pai(adam,pedro). % significa adam é pai de pedro
-    pai(adam,paulo).
+    progenitor(adam,pedro).
+    progenitor(adam,paulo).
+    progenitor(adam,luisa).
+    progenitor(eva,luisa).
+    progenitor(eva,pedro).
+    progenitor(eva,maria).
+    progenitor(martha,suzana).
+    irmao(pedro,luisa).
+    irmao(paulo,pedro).
+    irmao(pedro,paulo).
+    irma(luisa,pedro).
+    irma(eva,martha).
+    irma(martha,eva).
+    irma(maria,pedro).
+    irma(luisa,paulo).
     marido(adam,eva).
     esposa(eva,adam).
-    mae(eva,luisa).
-    mae(eva,pedro).
     filha(maria,eva).
     filha(luisa,adam).
     filha(suzana,martha).
-    irma(eva,martha).
-
-
-
- % REGRAS
 
     filho(F,P):-homem(F),pai(P,F).
     filho(F,M):-homem(F),mae(M,F).
-    
+
     filha(F,P):-mulher(F),pai(P,F).
     filha(F,M):-mulher(F),mae(M,F).
 
-% Completar as regras:
     irma(X,Y) :- progenitor(A,X), progenitor(A,Y),not(X=Y),mulher(X).
     irmao(X,Y) :-progenitor(A,X), progenitor(A,Y),not(X=Y),homem(X).
 
     prima(A,B) :- progenitor(T,X),progenitor(T,Y),progenitor(X,A),progenitor(Y,B),mulher(A).
     tia(A,B) :- progenitor(K,T),progenitor(K,A),progenitor(T,B),mulher(A).
-    
+
+
